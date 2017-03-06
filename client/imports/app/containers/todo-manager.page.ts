6bcb1f93beb0292df19ca2b7c9fd87434f80f890
Cory Forward <cory@linux.com>
2017-03-06 02:34:56 +0000

    backup

---
+++ client/imports/app/containers/todo-manager.page.ts
@@ -0,0 +1,38 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import * as fromRoot from '../reducers';
+import { Todo } from '@shared:models';
+
+
+@Component({
+  selector: 'todo-manager-page',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <md-card>
+      <md-card-title>Todo Manager</md-card-title>
+    </md-card>
+    <todo-add-form></todo-add-form>
+    <todo-list [todos]="todos$ | async"></todo-list>
+  `,
+  /**
+   * Container components are permitted to have just enough styles
+   * to bring the view together. If the number of styles grow,
+   * consider breaking them out into presentational
+   * components.
+   */
+  styles: [`
+    md-card-title {
+      display: flex;
+      justify-content: center;
+    }
+  `]
+})
+export class TodoManagerPage {
+  books$: Observable<Todo[]>;
+
+  constructor(store: Store<fromRoot.State>) {
+    this.books$ = store.select(fromRoot.getTodos);
+  }
+}
